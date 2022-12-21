8d296282be1e7e03847279544d4186a1e4384235
GadMinz <baykov97@mail.ru>
2022-12-22 00:29:06 +0300

    Add delete element

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import Element from "./components/Element";
 
+type TElement = {
+  id: string;
+};
+
 const App = () => {
+  const [items, setItems] = React.useState<TElement[]>([]);
+
+  const addItem = () => {
+    const id = new Date().valueOf().toString();
+    setItems((prevState) => [...prevState, { id }]);
+  };
+
+  const deleteItem = (id: string) => {
+    if (window.confirm("Delete element?")) {
+      setItems((prevState) => prevState.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <div className="wrapper">
-      <div className='create'>
-        <button>Create</button>
+      <div className="create">
+        <button onClick={addItem}>Create</button>
       </div>
-        <ol className='list'>
-            <Element />
-        </ol>
+      <ol className="list">
+        {items.length > 0 ? (
+          items.map((item) => (
+            <Element key={item.id} id={item.id} deleteItem={deleteItem} />
+          ))
+        ) : (
+          <div>Список пуст</div>
+        )}
+      </ol>
     </div>
   );
 };
--- src/components/Element/index.tsx
+++ src/components/Element/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import s from './Element.module.scss'
+import React from "react";
+import s from "./Element.module.scss";
 interface ElementProps {
-
+  id: string;
+  deleteItem: (id: string) => void;
 }
 
-
-const Element: React.FC<ElementProps> = () => {
-    return (
-        <li className={s.element}>
-            Исчезнет через N секунд
-        </li>
-    );
+const Element: React.FC<ElementProps> = ({ id, deleteItem }) => {
+  return (
+    <div className={s.wrapper}>
+      <li className={s.element}>Исчезнет через N секунд</li>
+      <button className={s.element_delete} onClick={() => deleteItem(id)}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+          <rect width="100%" height="100%" fill="none" />
+          <g className="currentLayer">
+            <path
+              fill="none"
+              stroke="#000"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1.164 4.506 4.55 1.083 8.144 4.72l3.595-3.637 3.384 3.423-3.595 3.638 3.595 3.637-3.384 3.424-3.595-3.637-3.595 3.637-3.385-3.424L4.76 8.144 1.164 4.506z"
+              color="#000"
+            />
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
 };
 
 export default Element;
